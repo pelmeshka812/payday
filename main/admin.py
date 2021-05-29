3397459abb36a1fe3c8b47b71d31eae22907f74d
pelmeshka812 <pelmeshka812@gmail.com>
2021-05-29 19:25:21 +0300

    Add  models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from main.models import Party, Product, Payroll, List_user
+
+admin.site.register(Party)
+admin.site.register(Product)
+admin.site.register(Payroll)
+admin.site.register(List_user)
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Party(models.Model):
+    creater = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='creater', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='participants')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    # party = models.ForeignKey(Party, on_delete=models.CASCADE)
+
+
+class Payroll(models.Model):
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+
+class List_user(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default='')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
